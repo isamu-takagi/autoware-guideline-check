e7484ace93c0a1399fbf1ec00cb4a41d87da1bc8
Takagi, Isamu <isamu.takagi@tier4.jp>
2024-11-29 16:23:28 +0900

    handle rviz and multifile
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- src/pre_commit_hooks/check_package_depends.py
+++ src/pre_commit_hooks/check_package_depends.py
@@ -2,6 +2,7 @@ import argparse
 import pathlib
 import re
 import xml.etree.ElementTree as etree
+import yaml
 
 
 def iter_depend_pkgs(filepath: pathlib.Path):
@@ -9,7 +10,7 @@ def iter_depend_pkgs(filepath: pathlib.Path):
     root = tree.getroot()
     pkgs = set()
     for node in root:
-        if node.tag.endswith("depend"):
+        if node.tag.endswith("depend") or node.tag == "name":
             pkgs.add(node.text)
     return pkgs
 
@@ -24,21 +25,52 @@ def list_launch_pkgs(filepath: pathlib.Path):
     return pkgs
 
 
+def list_rviz_pkgs(filepath: pathlib.Path):
+    pkgs = set()
+    def traverse(nodes):
+        nonlocal pkgs
+        nodes = nodes if nodes else []
+        nodes = nodes if type(nodes) == list else [nodes]
+        for node in nodes:
+            pkgs.add(node["Class"])
+            if node["Class"] == "rviz_common/Group":
+                traverse(node["Displays"])
+
+    for path in filepath.parent.glob("**/*.rviz"):
+        with path.open() as fp:
+            data = yaml.safe_load(fp)
+        traverse(data["Panels"])
+        traverse(data["Visualization Manager"]["Displays"])
+        traverse(data["Visualization Manager"]["Tools"])
+        traverse(data["Visualization Manager"]["Views"]["Current"])
+        traverse(data["Visualization Manager"]["Views"]["Saved"])
+
+    pkgs = {pkg for pkg in pkgs if not pkg.startswith("rviz_common/")}
+    pkgs = {pkg for pkg in pkgs if not pkg.startswith("rviz_default_plugins/")}
+
+    for pkg in pkgs:
+        print("    " + pkg)
+    return pkgs
+
 def main(argv = None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*')
     args = parser.parse_args(argv)
 
+    result = 0
     for filename in args.filenames:
         filepath = pathlib.Path(filename)
 
         depend_pkgs = iter_depend_pkgs(filepath)
-        launch_pkgs = list_launch_pkgs(filepath)
+        launch_pkgs = set()
+        launch_pkgs |= list_launch_pkgs(filepath)
 
         launch_pkgs = {pkg for pkg in launch_pkgs if "$" not in pkg}
         result_pkgs = launch_pkgs - depend_pkgs
 
-        for pkg in result_pkgs:
-            print(pkg)
-
-    return 1 if pkg else 0
+        if result_pkgs:
+            result = 1
+            print(filepath)
+            for pkg in result_pkgs:
+                print(f"  exec_depend: {pkg}")
+    return result
