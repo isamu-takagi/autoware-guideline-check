49d4936a336f9e3369bd27a380a1e66ae759e40f
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-08-07 21:03:39 +0900

    handle include
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- src/autoware_guideline_check/check_launch_file.py
+++ src/autoware_guideline_check/check_launch_file.py
@@ -1,47 +1,92 @@
-import pathlib
 import collections
-from typing import Text, Dict, Any
+import pathlib
+from typing import Any, Dict, Text
 
-from launch import Event
-from launch import LaunchDescription
-from launch import Substitution
-from launch.actions import IncludeLaunchDescription
+from launch import Event, Substitution
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_context import LaunchContext
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 
 
 class DummyContext:
-  def __init__(self):
-    self.launch_configurations = {}
+    def __init__(self):
+        self.__context = LaunchContext()
+        self.launch_configurations = {}
 
-  def perform_substitution(self, substitution: Substitution) -> Text:
-    print(" - perform_substitution", substitution)
-    return substitution.perform(self)
+    @property
+    def is_shutdown(self):
+        return True
 
-  def would_handle_event(self, event: Event) -> bool:
-    print(" - would_handle_event", event)
-    return False
+    @property
+    def environment(self):
+        return self.__context.environment
 
-  def extend_locals(self, extensions: Dict[Text, Any]) -> None:
-    print(" - extend_locals", extensions)
+    @property
+    def locals(self):
+        return self.__context.locals
+
+    def perform_substitution(self, substitution: Substitution) -> Text:
+        if not isinstance(substitution, TextSubstitution):
+            print(" - perform_substitution", substitution.describe())
+        return substitution.perform(self)
+
+    def would_handle_event(self, event: Event) -> bool:
+        # print(" - would_handle_event", event)
+        return False
+
+    def extend_locals(self, extensions: Dict[Text, Any]) -> None:
+        # print(" - extend_locals", extensions)
+        self.__context.extend_locals(extensions)
+
+    def _push_environment(self):
+        pass
+
+    def _pop_environment(self):
+        pass
+
+    def _push_launch_configurations(self):
+        pass
+
+    def _pop_launch_configurations(self):
+        pass
 
 
-def aaaa(ld: LaunchDescription):
-  context = DummyContext()
-  entities = collections.deque([ld])
-  while entities:
-    entity = entities.popleft()
-    print(entity)
-    result = entity.visit(context)
-    entities.extend(result if result else [])
+class DummyService:
+    def __init__(self):
+        self.entities = collections.deque()
+
+    def include_launch_file(self, path):
+        entities = IncludeLaunchDescription(str(path)).describe_sub_entities()
+        for entity in entities:
+            self.entities.extend(entity.describe_sub_entities())
+
+    def run(self):
+        context = DummyContext()
+        while self.entities:
+            entity = self.entities.popleft()
+            print(entity)
+            if isinstance(entity, IncludeLaunchDescription):
+                print(" - Skip include launch description")
+                continue
+            if isinstance(entity, DeclareLaunchArgument):
+                if entity.default_value is None:
+                    context.launch_configurations[entity.name] = entity.default_value
+            result = entity.visit(context)
+            self.entities.extend(result if result else [])
 
 
 def process_file(path: pathlib.Path, args):
-    ld = LaunchDescription([IncludeLaunchDescription(path)])
-    aaaa(ld)
+    service = DummyService()
+    service.include_launch_file(path)
+    service.run()
     return 0
+
 
 def main(argv=None):
     from .utils import EntryPoint
+
     return EntryPoint().main(process_file, argv)
+
 
 if __name__ == "__main__":
     process_file("test.launch.xml", {})
